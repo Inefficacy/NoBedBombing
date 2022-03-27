60a05c65eccbb6e2d364008b78185d32d6065947
Inefficacy <inefficacyfr@gmail.com>
2022-03-27 15:37:21 -0600

    Commit

--- src/me/inefficacy/nobedbombing/events/Events.java
+++ src/me/inefficacy/nobedbombing/events/Events.java
@@ -2,9 +2,9 @@ package me.inefficacy.nobedbombing.events;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
@@ -12,13 +12,15 @@ public class Events implements Listener {
 	
 	@EventHandler
 	public static void onDamageEvent(EntityDamageEvent e) {
-		if(e instanceof Player) {
-			Entity entity = e.getEntity();
-			if(e.getCause() == DamageCause.BLOCK_EXPLOSION) {
-				e.setDamage(0);
-			} else if (e.getCause() == DamageCause.ENTITY_EXPLOSION && entity.getType() == EntityType.ENDER_CRYSTAL) {
-				e.setDamage(0);
-			}
+		if(e.getCause() == DamageCause.BLOCK_EXPLOSION) {
+			e.setDamage(0);
+		}
+	}
+	@EventHandler
+	public static void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+		Entity entity = e.getDamager();
+		if (entity.getType() == EntityType.ENDER_CRYSTAL) {
+			e.setDamage(0);
 		}
 	}
 }
