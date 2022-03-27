e028f310b2fb783a30288308804f4d83761bdc8a
Inefficacy <inefficacyfr@gmail.com>
2022-03-27 14:31:00 -0600

    Initial Commit

---
+++ src/me/inefficacy/nobedbombing/NoBedBombing.java
@@ -0,0 +1,16 @@
+package me.inefficacy.nobedbombing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoBedBombing extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
