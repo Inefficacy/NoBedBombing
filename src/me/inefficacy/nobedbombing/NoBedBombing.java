c2316d5bc02f4e67306580e20a30d52aa941f6d1
Inefficacy <inefficacyfr@gmail.com>
2022-03-27 14:35:49 -0600

    Commit

--- src/me/inefficacy/nobedbombing/NoBedBombing.java
+++ src/me/inefficacy/nobedbombing/NoBedBombing.java
@@ -1,16 +1,17 @@
 package me.inefficacy.nobedbombing;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoBedBombing extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN+"NoBedBombing plugin has been enabled");
 	}
 	
 	@Override
 	public void onDisable() {
-		
+		getServer().getConsoleSender().sendMessage(ChatColor.GREEN+"NoBedBombing plugin has been disabled");
 	}
 }
